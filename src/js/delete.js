3267fa2a4d6dd1f062802ae1365cc8d425581f43
Kamal578 <ahmadov.kamal423@gmail.com>
2023-12-16 23:10:50 +0400

    modified deletion prompts

--- src/js/delete.js
+++ src/js/delete.js
@@ -1,5 +1,5 @@
 function confirmDelete(transactionID) {
-    if (confirm("Are you sure you want to delete this transaction?")) {
+    if (confirm("This action will delete this transaction PERMANENTLY. Are you sure?")) {
         window.location.href = "api/delete.php?transactionID=" + transactionID;
     }
     else{
@@ -8,7 +8,7 @@ function confirmDelete(transactionID) {
 }
 
 function confirmDeleteCategory(categoryID) {
-    if (confirm("Are you sure you want to delete this category?")) {
+    if (confirm("This action will delete this category and all of the associated transactions PERMANENTLY. Are you sure?")) {
         window.location.href = "api/delete_category.php?categoryID=" + categoryID;
     } else {
         return false;
@@ -16,7 +16,7 @@ function confirmDeleteCategory(categoryID) {
 }
 
 function confirmDeletePaymentMethod(paymentMethodID) {
-    if (confirm("Are you sure you want to delete this payment method?")) {
+    if (confirm("This action will delete this payment method and all of the associated transactions PERMANENTLY. Are you sure?")) {
         window.location.href = "api/delete_payment.php?paymentID=" + paymentMethodID;
     } else {
         return false;
